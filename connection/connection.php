54038ef3899a637edf18731507a52bbbab0e1e62
joan_ndambiri <joanevans18@gmail.com>
2021-09-13 18:55:56 +0300

    db connection

--- connection/connection.php
+++ connection/connection.php
@@ -1,5 +1,6 @@
 <?php 
-	$con=mysqli_connect("localhost","root","","imperial_college");
+	$con=mysqli_connect("aa14bpxfikgbsn4.c5hd6ydzohf5.eu-west-3.rds.amazonaws.com
+	","admin","cafescript","ebdb");
 	if(!$con)
 	{
 		echo "Connection is not Successfully";
